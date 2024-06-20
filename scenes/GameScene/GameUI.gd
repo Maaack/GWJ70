9e0f0bf270ef0643aa8dd04204db95e89c95c5fe
Maaack <marek.belski@gmail.com>
2024-06-20 13:47:44 -0700

    Updates game UI and main menu links back.

--- scenes/GameScene/GameUI.gd
+++ scenes/GameScene/GameUI.gd
@@ -1,27 +1,14 @@
 extends Control
 
-@export var win_scene : PackedScene
-@export var lose_scene : PackedScene
+var _user_status : String
+var _user_folklore : Array
 
 func _ready():
 	InGameMenuController.scene_tree = get_tree()
+	$GetFolkloreUser.request()
 
-func _on_level_lost():
-	InGameMenuController.open_menu(lose_scene, get_viewport())
-
-func _on_level_won():
-	$LevelLoader.advance_and_load_level()
-
-func _on_level_loader_level_loaded():
-	await $LevelLoader.current_level.ready
-	if $LevelLoader.current_level.has_signal("level_won"):
-		$LevelLoader.current_level.level_won.connect(_on_level_won)
-	if $LevelLoader.current_level.has_signal("level_lost"):
-		$LevelLoader.current_level.level_lost.connect(_on_level_lost)
-	$LoadingScreen.close()
-
-func _on_level_loader_levels_finished():
-	InGameMenuController.open_menu(win_scene, get_viewport())
-
-func _on_level_loader_level_load_started():
-	$LoadingScreen.reset()
+func _on_get_folklore_user_user_received(user_status, folklore):
+	_user_status = user_status
+	_user_folklore = folklore
+	%UserStatusLabel.text = "You are currently %s" % _user_status
+	%WaitingUI.folklore_data = _user_folklore
