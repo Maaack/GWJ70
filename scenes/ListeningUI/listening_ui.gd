6aee65c7052ff55c5496107f645db55c4827fb72
Maaack <marek.belski@gmail.com>
2024-06-21 17:59:01 -0700

    Adds story title and author name to display.

--- scenes/ListeningUI/listening_ui.gd
+++ scenes/ListeningUI/listening_ui.gd
@@ -3,10 +3,14 @@ extends Control
 
 @onready var past_folklore_player : AudioPlayer = %AudioPlayer
 @onready var folklore_transcript_label : RichTextLabel = %FolkloreTranscriptLabel
+@onready var story_title_label : Label = %StoryTitleLabel
+@onready var author_name_label : Label = %AuthorNameLabel
 
 
 func _on_past_folklore_controller_folklore_queued(audio_stream, story_name, author_name, transcript):
 	past_folklore_player.stop()
 	past_folklore_player.audio_stream = audio_stream
+	story_title_label.text = story_name
+	author_name_label.text = author_name
 	past_folklore_player.play()
 	folklore_transcript_label.text = "[right]%s[/right]" % transcript
